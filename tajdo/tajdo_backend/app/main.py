877e9c045da2a650d220736a1c5dce48bbad12ec
sheringugi <sheri.ngugi17@gmail.com>
2026-02-20 18:29:16 +0300

    update .env

--- tajdo/tajdo_backend/app/main.py
+++ tajdo/tajdo_backend/app/main.py
@@ -34,6 +34,7 @@ if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_origin_regex=r"https://tadjo-frontend.*\.vercel\.app",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
